ff69a548a31dc2f5dc2a16a0c70b042c8013b838
alec t <alec@alect.me>
2023-01-04 21:21:42 -0500

    Add Product component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+/* Components */
+import Product from './components/Product';
+
+/* Interfaces */
+import ProductProps from './interfaces/ProductProps';
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const productsURL = 'https://dummyjson.com/products';
+
+  useEffect(() => {
+    fetch(productsURL)
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.products);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  function renderProduct(product: ProductProps) {
+    return (
+      <Product
+        key={`${product.id}${product.title}`}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        price={product.price}
+        discountPercentage={product.discountPercentage}
+        rating={product.rating}
+        stock={product.stock}
+        brand={product.brand}
+        category={product.category}
+        thumbnail={product.thumbnail}
+        images={product.images}
+      />
+    )
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="products-container">
+        {products.map(renderProduct)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,32 @@
+/* Interfaces */
+import ProductProps from '../interfaces/ProductProps';
+
+function Product(props: ProductProps) {
+
+  function truncateText(text: string) {
+    const characters = 28;
+    return text.length <= characters ? text : `${text.slice(0, characters)}...`;
+  }
+
+  return (
+    <div className="product">
+      <img className="product-image" src={props.thumbnail} />
+      <div className="product-info">
+        <div className="product-brand">
+          {props.brand}
+        </div>
+        <div className="product-title">
+          {truncateText(props.title)}
+        </div>
+        <div className="product-description">
+          {truncateText(props.description)}
+        </div>
+        <div className="product-price">
+          {`$${props.price}`}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
